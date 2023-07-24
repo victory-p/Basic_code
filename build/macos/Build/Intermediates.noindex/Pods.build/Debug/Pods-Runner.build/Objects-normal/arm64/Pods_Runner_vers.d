14eee2931408dee5b271bcd195dcd15c2751efc5
hzlloni <jhw030329@gmail.com>
2023-07-24 16:44:11 +0900

    create files

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jeonhyewon/AndroidStudioProjects/Basic_code/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/jeonhyewon/AndroidStudioProjects/Basic_code/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
