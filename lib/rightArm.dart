89d1336da038bc1552b102d0045e732e6f9b887e
Hakyoungan <128044721+Hakyoungan@users.noreply.github.com>
2023-07-28 11:59:46 +0900

    Create rightArm.dart

---
+++ lib/rightArm.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:hellonong/util/color_schemes.g.dart';
+import 'package:hellonong/widget/bottomNavi.dart';
+import 'package:hellonong/widget/test.dart';
+
+import 'home.dart';
+import 'main.dart';
+import 'mypage.dart';
+import 'bag.dart';
+
+class  rightArm extends StatefulWidget {
+  const rightArm ({super.key});
+
+
+  @override
+  State<rightArm> createState() => _rightArmState();
+}
+
+class _rightArmState extends State<rightArm> {
+  Test test = Test(1);
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: [
+          IconButton(
+              onPressed: (){
+                Navigator.push(context,MaterialPageRoute(builder: (context) => Bag() //page 간 이동
+                ));},
+              icon:Icon(
+                Icons.list_alt_rounded,
+                size: 40,
+                color: Colors.white,
+              )
+          ),
+          IconButton(
+              onPressed: (){Navigator.push(context,MaterialPageRoute(builder: (context) => MyPage()));},
+              icon:Icon(
+                Icons.person_outline,
+                size: 40,
+                color: Colors.white,
+              )
+          )
+        ],
+      ),
+      body:
+      Align(
+        alignment: Alignment.center, // 가운데 정렬
+        child: Container(
+          //왼쪽 팔
+          height: screenHeight * 0.75,
+          width: screenWidth * 0.9,
+          margin: EdgeInsets.only(top: screenHeight * 0.01),
+          child: Stack(
+            children: [
+              // 사진을 가장 먼저 추가
+              Container(
+                width: screenWidth * 1,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/rightArm1.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              //어깨
+
+              Positioned(
+                bottom: screenHeight * 0.555,
+                left: screenWidth * 0.028,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()), // Replace Page3 with the desired page
+                    );
+                  },
+                child: Container(
+                  width: screenWidth * 0.33,
+                  height: screenHeight * 0.13,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Color.fromRGBO(102, 102, 102, 1.0),
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                ),
+              ),
+              // 팔위
+              Positioned(
+                bottom: screenHeight * 0.345,
+                left: screenWidth * 0.13,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()), // Replace Page3 with the desired page
+                    );
+                  },
+                child: Transform.rotate(
+                  angle: 1.1,
+                  child: Container(
+                    width: screenWidth * 0.45,
+                    height: screenHeight * 0.17,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Color.fromRGBO(102, 102, 102, 1.0),
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  ),
+                ),
+              ),
+              // 팔밑
+              Positioned(
+                bottom: screenHeight * 0.15,
+                left: screenWidth * 0.35,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()), // Replace Page3 with the desired page
+                    );
+                  },
+                child: Transform.rotate(
+                  angle: 1.1, // 회전 각도 (라디안 단위, 여기서는 -0.5 라디안 = -28.65도)
+                  child: Container(
+                    width: screenWidth * 0.39,
+                    height: screenHeight * 0.17,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Color.fromRGBO(102, 102, 102, 1.0),
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                ),
+                ),
+              ),
+
+              // 손
+              Positioned(
+                bottom: screenHeight * 0.01,
+                left: screenWidth * 0.61,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()), // Replace Page3 with the desired page
+                    );
+                  },
+                  child: Container(
+                    width: screenWidth * 0.27,
+                    height: screenHeight * 0.13,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Color.fromRGBO(102, 102, 102, 1.0),
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: test.bottmNavi(context),
+    );
+  }
+}
