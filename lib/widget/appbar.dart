5c76fe4fd4c3dbd036e69718ea86b7d7a1534960
Hajin <22100228@handong.ac.kr>
2023-08-07 10:34:23 +0900

    edit appbar.dart

--- lib/widget/appbar.dart
+++ lib/widget/appbar.dart
@@ -9,7 +9,9 @@ class CustomAppBar extends AppBar {
 
   CustomAppBar(this._buttonIndex, this.selectedIndex, this.context)
       : super(
-    leading: IconButton(
+    leading: _buttonIndex == 1 // Check if _buttonIndex is 1
+        ? null // Set leading to null if _buttonIndex is 1 to hide the back button
+        : IconButton(
       onPressed: () {
         // Navigate back when the back button is pressed.
         Navigator.pop(context);
@@ -22,50 +24,51 @@ class CustomAppBar extends AppBar {
     ),
     backgroundColor: Theme.of(context).colorScheme.primary,
     actions: [
-      IconButton(
-        onPressed: () {
-          // Navigate to the Bag page when the button is pressed.
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Bag()),
-          );
-        },
-        icon: Container(
-          width: 42, // 이미지의 너비
-          height: 42, // 이미지의 높이
-          child: Stack(
-            children: [
-              Image.asset(
-                'assets/images/list.png', // 리스트 아이콘 이미지 파일의 경로
-                width: 42, // 이미지의 너비
-                height: 42, // 이미지의 높이
-              ),
-              Positioned(
-                top: 0, // 동그라미의 위치를 조정하여 겹치도록 설정
-                right: 0, // 동그라미의 위치를 조정하여 겹치도록 설정
-                child: Container(
-                  width: 20, // 동그라미의 너비와 높이 설정 (원하는 크기로 조정)
-                  height: 20,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color(0xFFB41212), // 진한 빨간색
-                  ),
-                  child: Center(
-                    child: Text(
-                      '0', // 동그라미 안에 들어갈 내용 (원하는 텍스트로 변경 가능)
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14, // 텍스트 크기 조정
-                        fontWeight: FontWeight.bold,
+      if (_buttonIndex != 1) // Check if _buttonIndex is not 1 to show the Bag icon
+        IconButton(
+          onPressed: () {
+            // Navigate to the Bag page when the button is pressed.
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Bag()),
+            );
+          },
+          icon: Container(
+            width: 42, // 이미지의 너비
+            height: 42, // 이미지의 높이
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/list.png', // 리스트 아이콘 이미지 파일의 경로
+                  width: 42, // 이미지의 너비
+                  height: 42, // 이미지의 높이
+                ),
+                Positioned(
+                  top: 0, // 동그라미의 위치를 조정하여 겹치도록 설정
+                  right: 0, // 동그라미의 위치를 조정하여 겹치도록 설정
+                  child: Container(
+                    width: 20, // 동그라미의 너비와 높이 설정 (원하는 크기로 조정)
+                    height: 20,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFB41212), // 진한 빨간색
+                    ),
+                    child: Center(
+                      child: Text(
+                        '0', // 동그라미 안에 들어갈 내용 (원하는 텍스트로 변경 가능)
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14, // 텍스트 크기 조정
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
       IconButton(
         onPressed: () {
           // Navigate to the Usage or other page based on the _buttonIndex value.
@@ -79,7 +82,7 @@ class CustomAppBar extends AppBar {
             // For example, add more conditions or navigate to different pages.
           }
         },
-        icon: _buttonIndex == 1
+        icon: selectedIndex == 1
             ? Icon(
           Icons.help,
           size: 40,
