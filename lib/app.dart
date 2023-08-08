c008fa65cf3ad7effbf41b860c36459439cd9273
hzlloni <jhw030329@gmail.com>
2023-08-08 16:16:34 +0900

    sym

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hellonong/util/color_schemes.g.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hellonong/widget/bottomNavi.dart';
+import 'package:provider/provider.dart';
 
 import 'body.dart';
 import 'home.dart';
@@ -13,17 +14,72 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: lightColorScheme,
-        useMaterial3: true,
+    return ChangeNotifierProvider<MyAppState>(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: lightColorScheme,
+          useMaterial3: true,
+        ),
+        routes: {
+          '/': (context) => MyHomePage(),
+          '/body': (context) => TestScreen(),
+          '/pharmacy': (context) => Pharmacy(
+                isSelected1: [],
+                isSelected2: [],
+                isSelected3: [],
+              ),
+          '/mypage': (context) => MyPage(),
+        },
       ),
-      routes: {
-        '/': (context) => MyHomePage(),
-        '/body': (context) => TestScreen(),
-        '/pharmacy': (context) => Pharmacy(isSelected1: [], isSelected2: [], isSelected3: []),
-        '/mypage': (context) => MyPage(),
-      },
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  List<String> wishs = [];
+  List<String> sym = [];
+  List<String> picture = [];
+  List<String> imageURLs = [];
+  List<String> ids = [];
+  int favoriteCount = 41;
+  List<String> wishids = [];
+
+  void toggleWish(
+    String name,
+    String price,
+    String description,
+    String imageURL,
+    String id,
+  ) {
+    if (wishs.contains(name)) {
+      wishs.remove(name);
+      sym.remove(price);
+      picture.remove(description);
+      imageURLs.remove(imageURL);
+      ids.remove(id);
+      //FirebaseFirestore.instance.collection('WiSH').doc(id).delete();
+    } else {
+      wishs.add(name);
+      sym.add(price);
+      picture.add(description);
+      imageURLs.add(imageURL);
+      ids.add(id);
+    }
+    notifyListeners();
+  }
+
+  void toggleFavorite() {
+    notifyListeners();
+  }
+
+  void incNum() {
+    favoriteCount++;
+    notifyListeners();
+  }
+
+  void decNum() {
+    favoriteCount--;
+    notifyListeners();
   }
 }
--- lib/bag.dart
+++ lib/bag.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:hellonong/symptoms.dart';
+import 'package:hellonong/body.dart';
+import 'package:hellonong/util/color_schemes.g.dart';
 import 'package:hellonong/widget/bottomNavi.dart';
 import 'package:hellonong/widget/test.dart';
 import 'package:hellonong/widget/appbar.dart';
 
-import 'main.dart';
-import 'body.dart';
-import 'mypage.dart';
+import 'model/model.dart';
 
 class Bag extends StatefulWidget {
-  const Bag({Key? key});
+  final List<Product> selectedProducts;
+
+  Bag({required this.selectedProducts});
 
   @override
   State<Bag> createState() => _BagState();
@@ -19,6 +20,10 @@ class _BagState extends State<Bag> {
   int _selectedIndex = 1;
 
   void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
     switch (index) {
       case 0:
         Navigator.pushNamed(context, '/');
@@ -35,6 +40,8 @@ class _BagState extends State<Bag> {
     }
   }
 
+  Test test = Test(1);
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -42,29 +49,85 @@ class _BagState extends State<Bag> {
 
     return Scaffold(
       appBar: CustomAppBar(0, 0, context),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 20),
-          child: Container(
-            width: screenWidth * 0.9,
-            height: screenHeight * 0.9,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: Color(0xFFC1C1C1),
-                width: 1,
-              ),
-            ),
-            child: Stack(
-              alignment: Alignment.topCenter,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Column(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 0),
-                  child: Image.asset(
-                    "assets/images/bag.png",
-                    width: screenWidth * 0.12,
-                    height: screenHeight * 0.095,
+                Container(
+                  width: screenWidth * 0.9,
+                  height: screenHeight * 0.9,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Color(0xFFC1C1C1),
+                      width: 1,
+                    ),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 0),
+                        child: Image.asset(
+                          "assets/images/bag.png",
+                          width: screenWidth * 0.12,
+                          height: screenHeight * 0.095,
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: GridView.builder(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, // 2열로 표시
+                            crossAxisSpacing: 8.0,
+                            mainAxisSpacing: 8.0,
+                          ),
+                          itemCount: widget.selectedProducts.length,
+                          itemBuilder: (context, index) {
+                            Product product = widget.selectedProducts[index];
+                            return Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                                side: BorderSide(color: Colors.black, width: 1.2),
+                              ),
+                              clipBehavior: Clip.antiAlias,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.fromLTRB(15.0, 3.0, 5.0, 5.0),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text(
+                                            product.name,
+                                            style: TextStyle(
+                                              fontSize: 14.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                            maxLines: 1,
+                                          ),
+                                          const SizedBox(height: 8.0),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  AspectRatio(
+                                    aspectRatio: 18 / 15,
+                                    child: Image.asset(
+                                      'assets/images/sym/cheek/${product.id}.png',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
@@ -72,53 +135,58 @@ class _BagState extends State<Bag> {
           ),
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          SizedBox(
-            width: 150,
-            height: 60,
-            child: FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TestScreen(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 150,
+                height: 60,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TestScreen(),
+                      ),
+                    );
+                  },
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 30,
                   ),
-                );
-              },
-              child: Icon(
-                Icons.add,
-                color: Colors.white,
-                size: 30,
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
               ),
-              backgroundColor: Theme.of(context).primaryColor,
-            ),
-          ),
-          SizedBox(width: 10),
-          SizedBox(
-            width: 150,
-            height: 60,
-            child: FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Symptoms(),
+              SizedBox(width: 10),
+              SizedBox(
+                width: 150,
+                height: 60,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TestScreen(),
+                      ),
+                    );
+                  },
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 30,
                   ),
-                );
-              },
-              child: Icon(
-                Icons.check,
-                color: Colors.white,
-                size: 30,
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
               ),
-              backgroundColor: Theme.of(context).primaryColor,
-            ),
+            ],
           ),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationWidget(
         selectedIndex: _selectedIndex,
         onItemTapped: _onItemTapped,
